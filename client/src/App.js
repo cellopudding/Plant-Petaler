3b388548d16787e8f2858929ceaab0bdf3c0ac04
bsimmonds28 <bsimmonds28@gmail.com>
2023-05-07 10:51:14 -0400

    app styling - needs further updating

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';

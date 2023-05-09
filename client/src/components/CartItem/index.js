e0997d7ed9cef8bf22d67cc485c3ef208a8943d9
Mxwsic <danielapacible2@gmail.com>
2023-05-08 21:28:00 -0400

    added things for stripe

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -35,7 +35,7 @@ const CartItem = ({ item }) => {
   return (
     <div className="flex-row">
       <div>
-        <img src={`/images/${item.image}`} alt="" />
+        <img src={`${item.image}`} alt="" />
       </div>
       <div>
         <div>

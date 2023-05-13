bf2f0f796f5d8fe92f70160b020a6582ed1d2632
Lonnie Root <root.lonnie@gmail.com>
2023-05-12 20:00:17 -0400

    readme update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
+//hello//
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
